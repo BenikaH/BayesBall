837a5f9183492c893caa3ba5a0f3f3678f02b325
chris-french <chris.french.writes@gmail.com>
2017-08-31 01:16:45 -0700

    Delete ninja.py

--- ninja.py
+++
@@ -1,8 +0,0 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-
-env = Environment(
-    loader=FileSystemLoader(searchpath="templates"),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('basic.html')
