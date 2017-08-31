315fa19681fdd068e12cd4fd8160086720b0b2f0
chris-french <chris.french.writes@gmail.com>
2017-08-31 01:17:04 -0700

    Delete priors.py

--- priors.py
+++
@@ -1,7 +0,0 @@
-
-from context import outcomes, actions
-from collections import namedtuple
-
-def get_action_prior(action, default=True):
-    fields = outcomes[actions.index(action)]
-    return namedtuple('Prior', fields)
